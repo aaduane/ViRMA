df39405755132e16377d30905a5be1c7db3369f1
Aaron Duane <aaron.duane24601@gmail.com>
2021-06-03 17:41:00 +0200

    finished implementing movement limits on DimEx

--- Assets/Scripts/ViRMA_DimExplorer.cs
+++ Assets/Scripts/ViRMA_DimExplorer.cs
@@ -16,7 +16,10 @@ public class ViRMA_DimExplorer : MonoBehaviour
 
     public Bounds dimExBounds;
 
-    private bool boundLimitChecker;
+    private Vector3 maxRight;
+    private Vector3 maxLeft;
+    private float dist1;
+    private float dist2;
 
     private void Awake()
     {
@@ -50,13 +53,10 @@ public class ViRMA_DimExplorer : MonoBehaviour
                 }
                 horizontalRigidbody.isKinematic = false;
 
-                if (!boundLimitChecker)
-                {
-                    Vector3 rightHandVelocity = transform.InverseTransformDirection(Player.instance.rightHand.GetTrackedObjectVelocity());
-                    rightHandVelocity.y = 0;
-                    rightHandVelocity.z = 0;
-                    horizontalRigidbody.velocity = transform.TransformDirection(rightHandVelocity);
-                }        
+                Vector3 rightHandVelocity = transform.InverseTransformDirection(Player.instance.rightHand.GetTrackedObjectVelocity());
+                rightHandVelocity.y = 0;
+                rightHandVelocity.z = 0;
+                horizontalRigidbody.velocity = transform.TransformDirection(rightHandVelocity);
             }
             else
             {
@@ -106,9 +106,15 @@ public class ViRMA_DimExplorer : MonoBehaviour
         Vector3 flattenedVector = Player.instance.bodyDirectionGuess;
         flattenedVector.y = 0;
         flattenedVector.Normalize();
-        Vector3 spawnPos = Player.instance.hmdTransform.position + flattenedVector * 0.5f;
+        Vector3 spawnPos = Player.instance.hmdTransform.position + flattenedVector * 0.6f;
         transform.position = spawnPos;
         transform.LookAt(2 * transform.position - Player.instance.hmdTransform.position);
+
+
+        float maxDistanceX = dimExBounds.extents.x * 1.1f;
+        Vector3 movement = transform.right * maxDistanceX;
+        maxRight = transform.position + movement;
+        maxLeft = transform.position - movement;
     }
 
     private void CalculateBounds()
@@ -120,36 +126,49 @@ public class ViRMA_DimExplorer : MonoBehaviour
             bounds.Encapsulate(mesh.bounds);
         }
 
-        dimExBounds = bounds;
-
-        Debug.Log("Calculating bounds...");
+        dimExBounds = bounds;     
     }
 
     private void DimExGroupLimiter()
     {
         if (Player.instance)
         {
+            //float distanceFromPlayer = Vector3.Distance(transform.position, Player.instance.hmdTransform.transform.position);
+
+
             Vector3 adjustVelocity = horizontalRigidbody.velocity;
 
-            float maxDistanceX = dimExBounds.extents.x;
+            int DimExPosChecker = 0;
 
-            if (Vector3.Distance(transform.position, Player.instance.hmdTransform.transform.position) > maxDistanceX)
+            float distTemp1 = Vector3.Distance(maxRight, transform.position);
+            if (distTemp1 < dist1)
             {
-
-                //adjustVelocity.x = 0;
-                //adjustVelocity.z = 0;
-
-                adjustVelocity.x = adjustVelocity.x * -0.2f;
-                adjustVelocity.z = adjustVelocity.z * -0.2f;
-
-                horizontalRigidbody.velocity = adjustVelocity;
-
-                boundLimitChecker = true;
+                dist1 = distTemp1;
             }
-            else
+            else if (distTemp1 > dist1)
             {
-                boundLimitChecker = false;
+                dist1 = distTemp1;
+                DimExPosChecker++;
             }
+
+            float distTemp2 = Vector3.Distance(maxLeft, transform.position);
+            if (distTemp2 < dist2)
+            {
+                dist2 = distTemp2;
+            }
+            else if (distTemp2 > dist2)
+            {
+                dist2 = distTemp2;
+                DimExPosChecker++;
+            }
+
+            if (DimExPosChecker > 1)
+            {
+                adjustVelocity = Vector3.zero;
+            }
+
+            horizontalRigidbody.velocity = adjustVelocity;
+
         }
     }
 
